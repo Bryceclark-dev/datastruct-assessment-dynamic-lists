72213b29d2601ba84718f33142f33f773ad10913
Bryceclark-dev <brycejclark@sbcglobal.net>
2022-06-05 17:35:41 -0500

    Complete assesment

--- src/main/java/com/kenzie/app/Main.java
+++ src/main/java/com/kenzie/app/Main.java
@@ -1,10 +1,42 @@
 package com.kenzie.app;
 
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.lang.StringBuilder;
 
 public class Main {
+
+    public static String formatAttendeeList(ArrayList<String> names){
+        StringBuilder nameList = new StringBuilder("You have invited: ");
+
+            if(names.size()==1) {
+                nameList.append(names.get(0));
+                return nameList.toString();
+            }
+
+            if(names.size()==2) {
+                for (int i = 0; i < 2; i++) {
+                    nameList.append(names.get(i));
+                    if (i < 1) {
+                        nameList.append(" and ");
+                    }
+                }
+                return nameList.toString();
+            }
+
+            for (int i = 0; i < names.size(); i++) {
+                nameList.append(names.get(i));
+                if (i < names.size() - 1) {
+                    nameList.append(", ");
+                }
+                if (i == names.size() - 2) {
+                    nameList.append("and ");
+                }
+            }
+
+        return nameList.toString();
+    }
 
     public static void main(String[] args) {
         // Step 1 - Print out a welcome message and instructions
@@ -25,5 +57,21 @@ public class Main {
         // Your output should exactly match the way these are formatted with spaces, commas, and the "and"
 
         // Your Code Here
+        ArrayList<String> attendees = new ArrayList<String>();
+        String names = " ";
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hello!  Who would you like to invite to the party?\n" +
+                            "Enter an attendee name and press Enter.\n" +
+                            "Leave the name blank and press enter when you are done.");
+
+        while(true){
+            names = scan.nextLine();
+            if(names.equals("")){
+                break;
+            }
+            attendees.add(names);
+        }
+        System.out.println(formatAttendeeList(attendees));
+
     }
 }
